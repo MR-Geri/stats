26040f1dac5198fb0d6a3ac0ae16eb35bbe87257
MR-Geri <ilya.kamckine@ya.ru>
2020-12-30 22:38:31 +0300

    update

---
+++ dist/stats/data/apps_open.py
@@ -0,0 +1,49 @@
+from data.keyboard import action_two_key, keyboard_layout
+
+import time
+import pygame
+import win32api
+import win32con
+
+
+def open_music():
+    action_two_key(win32con.VK_LWIN, 0x51)
+    keyboard_layout('68748313')
+    for ind in [0x5A, 0x59, 0x4C, 0x54, 0x52, 0x43, 0xBF, 0x56, 0x45, 0x50, 0x53, 0x52, 0x46, 0x0D]:
+        win32api.keybd_event(ind, 0, win32con.KEYEVENTF_EXTENDEDKEY, 0)
+        win32api.keybd_event(ind, 0, win32con.KEYEVENTF_EXTENDEDKEY | win32con.KEYEVENTF_KEYUP, 0)
+        time.sleep(0.001)
+    time.sleep(0.2)
+    win32api.keybd_event(win32con.VK_RETURN, 0, win32con.KEYEVENTF_EXTENDEDKEY, 0)
+    win32api.keybd_event(win32con.VK_RETURN, 0, win32con.KEYEVENTF_EXTENDEDKEY | win32con.KEYEVENTF_KEYUP, 0)
+
+
+def get_app(path='data/path.txt'):
+    """
+    Создание кнопок для открытия
+    :param path: путь до файла с приложениями
+    :return: список всех приложений для кнопок
+    """
+    apps = []
+    with open(path, encoding='utf-8', mode='r') as file:
+        data = [
+            [i.split('; ') for i in line.split('\n')[1:-1]]
+            for line in file.read().split(f'-' * 5)
+        ]
+        for line_ind in range(len(data)):
+            for app_ind in range(len(data[line_ind])):
+                if_path = eval(data[line_ind][app_ind][3])
+                apps.append(
+                    {
+                        'name': data[line_ind][app_ind][0],
+                        'x': 27 + 60 * app_ind,
+                        'y': 355 + 60 * line_ind,
+                        'icon': pygame.image.load(data[line_ind][app_ind][1]),
+                        'os_open': fr'{data[line_ind][app_ind][2]}' if if_path else eval(data[line_ind][app_ind][2]),
+                        'if_os_open_name': if_path
+                    }
+                )
+    return apps
+
+
+APP = get_app()
---
+++ dist/stats/data/run_line.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def run_line():
+    running_line = ['' for _ in range(2)]
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/85.0.4183.102 YaBrowser/20.9.2.102 Yowser/2.5 Safari/537.36',
+               'accept': '*/*'}
+    data_soup = []
+    for url in ['usd', 'eur']:
+        soup = BeautifulSoup(requests.get(f'https://mainfin.ru/currency/{url}/lipeck',
+                                          headers=headers).text,
+                             'html.parser')
+        data_soup.append(f'{float(soup.find_all("span", class_="float-convert__btn")[1].get_text()):.2f}')
+    try:
+        text = ' ' * (7 - len(data_soup[0]) - len(data_soup[1])) + f'$ = {data_soup[0]}  € = {data_soup[1]}'
+    except:
+        text = ''
+    running_line[1] = [[[(text, 60, 140)], (255, 255, 255)]]
+    soup = BeautifulSoup(requests.get('https://world-weather.ru/pogoda/russia/lipetsk/', headers=headers).text,
+                         'html.parser')
+    # Ощущается как °C
+    # Вероятность осадков %
+    # Давление мм рт. ст.
+    # Скорость ветра м/с
+    # Влажность воздуха
+    items = soup.find_all('div', class_='pane')
+    data = [[['' for _ in range(5)], ()] for _ in range(4)]
+    col = [(35, 255, 0), (0, 0, 255), (255, 255, 0), (128, 0, 255)]
+    data_find = ['weather-feeling', 'weather-probability', 'weather-pressure', 'weather-wind', 'weather-humidity']
+    for i in range(4):
+        for f in data_find:
+            s = ''
+            for te in items[i].find_all('td', class_=f):
+                s += te.get_text(strip=True) + ' '
+            data[i][0][data_find.index(f)] = (s.rstrip(), 67 + 8 * (19 - len(s.rstrip())), 140)
+        data[i][1] = col[i]
+    running_line[0] = data
+    return running_line
